cfb394a9f75ea1ac76ec4d5261121bff8628e01e
Yi <101046167+YiYiYiYiYiYiYiYiYiYiYi@users.noreply.github.com>
2022-06-19 18:02:21 +0200

    created a button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-     Hello
+        <a href="/" className="btn btn-primary shadow">Hello</a>
       </header>
     </div>
   );
