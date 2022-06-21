628c6539283cae2f91cbcb2ae35a6573bedf57aa
Yi <101046167+YiYiYiYiYiYiYiYiYiYiYi@users.noreply.github.com>
2022-06-21 20:08:53 +0200

    created basic form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className="App">
+      <div className='container'>
       <header className="App-header">
-        <a href="/" className="btn btn-primary shadow">Hello</a>
+        
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className='App-footer'>
+        <small>
+        Coded by Yi
+        </small>
+      </footer>
+      </div>
     </div>
   );
 }
