9d60c43b400c8988c979d3b8a037c5c0f045ba27
Yi <101046167+YiYiYiYiYiYiYiYiYiYiYi@users.noreply.github.com>
2022-06-29 21:30:22 +0200

    added styling

--- src/Results.js
+++ src/Results.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Meaning from "./Meaning";
 import Phonetics from "./Phonetics";
-import "./Results.css"
+import "./Results.css";
 
 
 export default function Results(props){
